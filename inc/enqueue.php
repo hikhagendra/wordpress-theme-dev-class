8d6d5665d8eb2a2ea874a39d7891b87f9043c20a
meroservice <info@meroservice.com>
2022-06-08 02:01:39 +0545

    Class 17

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -34,14 +34,14 @@ add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts' );
 //     =======================
 
 function sunset_load_scripts() {
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '5.2.0', 'all' );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all' );
     wp_enqueue_style( 'sunset', get_template_directory_uri() . '/css/sunset.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'raleway', 'https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;500&display=swap' );
 
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.6.0.min.js', false, '3.6.0', true );
     wp_enqueue_script( 'jquery' );
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '5.2.0', true );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'sunset_load_scripts' );
