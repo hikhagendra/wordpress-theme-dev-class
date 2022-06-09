c82824341fbf2d3333250debbb476e0208f07560
meroservice <info@meroservice.com>
2022-06-10 03:18:55 +0545

    Class 21

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -67,10 +67,10 @@ function sunset_posted_footer() {
             $comments = __( '1 Comment' );
         }
 
-        $comments = '<a href="'. get_comments_link() .'">'. $comments .'<span class="sunset-icon sunset-comment"></span></a>';
+        $comments = '<a href="'. get_comments_link() .'" class="comments-link">'. $comments .'<span class="sunset-icon sunset-comment"></span></a>';
     } else {
         $comments = __( 'Comments are closed' );
     }
 
-    return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list( '<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>' ) .'</div><div class="col-xs-12 col-sm-6">'. $comments .'</div></div></div>';
+    return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list( '<div class="tags-list"><span class="sunset-icon sunset-tag"></span>', ' ', '</div>' ) .'</div><div class="col-xs-12 col-sm-6 text-right">'. $comments .'</div></div></div>';
 }
