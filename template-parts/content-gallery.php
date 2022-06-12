b21c2e7ef407bda88bb637a9f7d491ccde8fdc61
meroservice <info@meroservice.com>
2022-06-12 20:27:00 +0545

    Class 27

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -11,32 +11,28 @@
 
     <header class="entry-header text-center">
 
-        <?php if ( sunset_get_attachment() ):
-            $attachments = sunset_get_attachment(7);
-            // var_dump($attachments); 
-        ?>
+        <?php if ( sunset_get_attachment() ): ?>
 
         <div id="post-gallery-<?php the_ID(); ?>" class="carousel slide sunset-carousel-thumb" data-ride="carousel">
          
             <div class="carousel-inner" role="listbox">
 
-                <?php 
-                    $count = count($attachments) - 1;
-                    for( $i = 0; $i <= $count; $i++ ):
-                    $active = ( $i == 0 ? 'active' : '');
+                <?php
+                    $attachments = sunset_get_bs_slides( sunset_get_attachment(7) );
 
-                    $n = ( $i == $count ? 0 : $i+1 );
-                    $nextImg = wp_get_attachment_thumb_url( $attachments[$n]->ID );
-                    $p = ( $i == 0 ? $count : $i-1 );
-                    $prevImg = wp_get_attachment_thumb_url( $attachments[$p]->ID );
+                    foreach( $attachments as $attachment ):
                 ?>
 
-                    <div class="item <?php echo $active; ?> background-image standard-featured" style="background-image: url(<?php echo wp_get_attachment_url( $attachments[$i]->ID ); ?>);">
-                        <div class="hide next-image-preview" data-image="<?php echo $nextImg; ?>"></div>
-                        <div class="hide prev-image-preview" data-image="<?php echo $prevImg; ?>"></div>
-                    </div>
+                <div class="item <?php echo $attachment['class']; ?> background-image standard-featured" style="background-image: url(<?php echo $attachment['url']; ?>);">
+                    <div class="hide next-image-preview" data-image="<?php echo $attachment['next_img']; ?>"></div>
+                    <div class="hide prev-image-preview" data-image="<?php echo $attachment['prev_img']; ?>"></div>
 
-                <?php endfor; ?>
+                    <div class="entry-excerpt image-caption">
+                        <p><?php echo $attachment['caption']; ?></p>
+                    </div>
+                </div>
+
+                <?php endforeach; ?>
 
             </div> <!-- .carousel-inner -->
 
